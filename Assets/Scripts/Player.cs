614cd3f859f52090638fd7d235febe08b4b05c08
ttanatb <ttanatb@gmail.com>
2018-02-20 18:21:27 -0500

    Fixed swaying logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,17 +10,22 @@ public class Player : MonoBehaviour
     float size = 5f;
 
     Vector3 lerpPos;
-    Vector3 prevPos;
+    Vector3 lastPos;
     public float ANIM_TIME = 1f;
     float timer;
     float swayAmount;
     public float SWAY_MAX = 2;
     Vector3 dir;
 
+    private Vector3 prevPos;
+    private Vector3 prevVel;
+    private Vector3 vel;
+
     // Use this for initialization
     void Start()
     {
         lerpPos = transform.position;
+        lastPos = lerpPos;
         prevPos = lerpPos;
         timer = ANIM_TIME;
         swayAmount = SWAY_MAX;
@@ -31,21 +37,30 @@ public class Player : MonoBehaviour
     {
         CheckInput();
         UpdatePos();
+        UpdateForward();
+    }
+
+    private void UpdateForward()
+    {
+        Quaternion prevRot = transform.rotation;
+        transform.rotation = Quaternion.Lerp(prevRot, Quaternion.Euler(0f, vel.x * 10f, 0f), Time.deltaTime * 10f);
     }
 
     void CheckInput()
     {
+        if (timer < ANIM_TIME) return;
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             laneIndex -= 1;
             if (laneIndex < -LANE_COUNT / 2)
             {
                 laneIndex = -LANE_COUNT / 2;
+                return;
             }
-            prevPos = transform.position;
+            lastPos = transform.position;
             lerpPos.x = laneIndex * size / 2f + Mathf.Sin(Time.time * 5f) / 5f;
             timer = 0f;
-            dir = Vector3.right;
+            dir = -Vector3.right;
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
@@ -53,25 +68,31 @@ public class Player : MonoBehaviour
             if (laneIndex > LANE_COUNT / 2)
             {
                 laneIndex = LANE_COUNT / 2;
+                return;
             }
-            prevPos = transform.position;
+            lastPos = transform.position;
             lerpPos.x = laneIndex * size / 2f;
             timer = 0f;
-            dir = -Vector3.right;
+            dir = Vector3.right;
         }
     }
 
     void UpdatePos()
     {
-        if (timer / ANIM_TIME < 1f)
+        prevPos = transform.position;
+        prevVel = vel;
+
+        if (timer < ANIM_TIME)
         {
-            transform.position = Vector3.Lerp(prevPos, lerpPos, timer / ANIM_TIME);
+            transform.position = Vector3.Lerp(lastPos, lerpPos, timer / ANIM_TIME);
         }
         else
         {
-            Vector3 swayVec = -dir * swayAmount;
-            transform.position = Vector3.Lerp(lerpPos - swayVec, lerpPos + swayVec, Mathf.Sin(timer / ANIM_TIME - 1f) / 2f + 0.5f);
+            Vector3 swayVec = dir * swayAmount;
+            transform.position = Vector3.Lerp(lerpPos - swayVec, lerpPos + swayVec, Mathf.Sin((timer - ANIM_TIME) * 3f) / 2f + 0.5f);
         }
+
+        vel = (transform.position - prevPos) / Time.deltaTime;
 
         timer += Time.deltaTime;
     }
